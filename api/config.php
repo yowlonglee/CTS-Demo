d8f4fbfcce77736cee450d58807b10a21cac1f07
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2024-08-24 02:42:06 +0800

    env vars

--- api/config.php
+++ api/config.php
@@ -5,7 +5,7 @@
 // $dbname = 'demo';
 
 // 建立連線
-$conn = new mysqli($host, $dbuser, $dbpassword, $dbname);
+$conn = new mysqli(host, dbuser, dbpassword, dbname);
 // 檢查連線
 if ($conn->connect_error) { die("連接失敗 : " . $conn->connect_error); }
 ?>
